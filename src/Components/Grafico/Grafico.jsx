b984b645d22abc2280bbdbe2538d5e5704474041
lipeqalves <lipeqa@gmail.com>
2022-10-02 22:13:30 -0300

    fix: ajusta os valores no grafico

--- src/Components/Grafico/Grafico.jsx
+++ src/Components/Grafico/Grafico.jsx
@@ -2,9 +2,7 @@ import { Chart } from "react-google-charts";
 import { Api } from "../../Service/Api";
 import { useState, useEffect } from "react";
 
-
 export const Grafico = () => {
-  const [alunos, setAlunos] = useState([]);
   const [rengeIdades1, setRengeIdades1] = useState([]);
   const [rengeIdades2, setRengeIdades2] = useState([]);
   const [rengeIdades3, setRengeIdades3] = useState([]);
@@ -13,40 +11,34 @@ export const Grafico = () => {
 
   useEffect(() => {
     Api.get("/alunos").then((res) => {
-      setAlunos(res.data);
       graficoIdade(res.data);
-    }); 
-
-  }, []);
-  
-  const graficoIdade = (alunoIdades) => {
+    });
     const idades1 = [];
     const idades2 = [];
     const idades3 = [];
     const idades4 = [];
     const idades5 = [];
-    alunoIdades.map((alunoIdade) => {
-      if (alunoIdade.idade >= 18 && alunoIdade.idade <= 21) {
-        idades1.push(parseInt(alunoIdade.idade));
-        setRengeIdades1(idades1);
-      } else if (alunoIdade.idade > 21 && alunoIdade.idade <= 25) {
-        idades2.push(parseInt(alunoIdade.idade));
-        setRengeIdades2(idades2);
-      } else if (alunoIdade.idade > 25 && alunoIdade.idade <= 29) {
-        idades3.push(parseInt(alunoIdade.idade));
-        setRengeIdades3(idades3);
-      } else if (alunoIdade.idade > 29 && alunoIdade.idade <= 33) {
-        idades4.push(parseInt(alunoIdade.idade));
-        setRengeIdades4(idades4);
-      } else {
-        idades5.push(parseInt(alunoIdade.idade));
-        setRengeIdades5(idades5);
-      }
-    });
-  };
+
+    const graficoIdade = (alunos) => {
+      alunos.map((alunoIdade) => {
+        if (alunoIdade.idade >= 18 && alunoIdade.idade <= 21) {
+          setRengeIdades1([idades1.push(parseInt(alunoIdade.idade))]);
+        } else if (alunoIdade.idade > 21 && alunoIdade.idade <= 25) {
+          setRengeIdades2([idades2.push(parseInt(alunoIdade.idade))]);
+        } else if (alunoIdade.idade > 25 && alunoIdade.idade <= 29) {
+          setRengeIdades3([idades3.push(parseInt(alunoIdade.idade))]);
+        } else if (alunoIdade.idade > 29 && alunoIdade.idade <= 33) {
+          setRengeIdades4([idades4.push(parseInt(alunoIdade.idade))]);
+        } else {
+          setRengeIdades5([idades5.push(parseInt(alunoIdade.idade))]);
+        }
+      });
+    };
+  });
+
   const mediaIdade = (rengeIdades) => {
     return rengeIdades.reduce((prev, corrent) => {
-      return (prev + corrent) / alunos.length;
+      return (prev + corrent) / rengeIdades.length;
     }, 0);
   };
   const options = {
@@ -67,7 +59,7 @@ export const Grafico = () => {
         ["de 22 a 25 anos", mediaIdade(rengeIdades2)],
         ["de 26 a 29 anos", mediaIdade(rengeIdades3)],
         ["De 30 a 33 anos", mediaIdade(rengeIdades4)],
-        ["Acima de 33 anos",mediaIdade(rengeIdades5)],
+        ["Acima de 33 anos", mediaIdade(rengeIdades5)],
       ]}
       options={options}
     />
